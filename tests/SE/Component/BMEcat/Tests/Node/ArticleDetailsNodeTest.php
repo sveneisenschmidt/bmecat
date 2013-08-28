b31be502833b7aecec0bd8183d0490813f2c3c7d
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2013-08-28 20:43:40 +0200

    xml test fixtures for ArticleDetailsNode

--- tests/SE/Component/BMEcat/Tests/Node/ArticleDetailsNodeTest.php
+++ tests/SE/Component/BMEcat/Tests/Node/ArticleDetailsNodeTest.php
@@ -17,6 +17,11 @@ namespace SE\Component\BMEcat\Tests\Node;
  */
 class ArticleDetailsNodeTest  extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+    }
+
     /**
      *
      * @test
@@ -93,7 +98,13 @@ class ArticleDetailsNodeTest  extends \PHPUnit_Framework_TestCase
      */
     public function Serialize_With_Null_Values()
     {
-        $this->markTestIncomplete();
+        $node = new \SE\Component\BMEcat\Node\ArticleDetailsNode();
+        $this->serializer->setSerializeNull(true);
+
+        $expected = file_get_contents(__DIR__.'/../Fixtures/empty_article_details_with_null_values.xml');
+        $actual = $this->serializer->serialize($node, 'xml');
+
+        $this->assertEquals($expected, $actual);
     }
 
     /**
@@ -102,6 +113,12 @@ class ArticleDetailsNodeTest  extends \PHPUnit_Framework_TestCase
      */
     public function Serialize_Without_Null_Values()
     {
-        $this->markTestIncomplete();
+        $node = new \SE\Component\BMEcat\Node\ArticleDetailsNode();
+        $this->serializer->setSerializeNull(false);
+
+        $expected = file_get_contents(__DIR__.'/../Fixtures/empty_article_details_without_null_values.xml');
+        $actual = $this->serializer->serialize($node, 'xml');
+
+        $this->assertEquals($expected, $actual);
     }
 } 
